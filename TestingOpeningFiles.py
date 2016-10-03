49863038b5a8c0e603f50dc38f937a6c4b339702
DunnyDon <conordonohue@gmail.com>
2016-10-03 17:49:59 +0100

    Opening and Parsing Airtel Data Files
    Opens and prints out data with their appropriate headers. Assumes that
    headers will always be the same and the order of data in the line will
    be the same.
    
    It also is able to decipher whether it is a USSD request or a response.

--- TestingOpeningFiles.py
+++ TestingOpeningFiles.py
@@ -1,12 +1,23 @@
 import os
-#os.chdir("D:\Airtel-Chad-USSD-MO-CDR's-6months")
 root = "D:\Airtel-Chad-USSD-MO-CDR's-6months"
+USSDRequestHeaders = ["SubscriberID","ServiceID","Transaction","Date","Time","CDRCorrelationID","MSISDN","IMSI","SessionID","USSDContent"]
+USSDResponseHeaders = ["SubscriberID","ServiceID","Transaction","Date","Time","CDRCorrelationID","MSISDN","SessionID","USSDContent","Status","ErrorCode"]
 for root, dirs, files in os.walk(root):
 	for FILENAME in files:
-		#make sure file is a log file and is called ProcessHistory
 		if not FILENAME.endswith('.gz'):
 			fd = open(root+"\\"+FILENAME, 'r')
 			#print(FILENAME)
 			#open it with read only privilages
 			for line in fd:
-				print line
+				count = 0
+				print line.split(",")[2]
+				if int(line.split(",")[2]) == 1:
+					for i in line.split(","):
+						print USSDRequestHeaders[count]," ",i
+						count+=1
+				else:
+					for i in line.split(","):
+						print USSDResponseHeaders[count]," ",i
+						count+=1
+					break
+			break
