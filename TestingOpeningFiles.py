5844f766e4bb06ba2fef132919a1e31e1ec006a8
DunnyDon <conordonohue@gmail.com>
2016-10-03 16:55:38 +0100

    Opening USSD Data Files
    Very preliminary test of opening and reading Airtel Data

---
+++ TestingOpeningFiles.py
@@ -0,0 +1,11 @@
+import os
+os.chdir("D:\Airtel-Chad-USSD-MO-CDR's-6months")
+for root, dirs, files in os.walk(root):
+	for FILENAME in files:
+		#make sure file is a log file and is called ProcessHistory
+		if not FILENAME.endswith('.gz'):
+			fd = open(root+"\\"+FILENAME, 'r')
+			#print(FILENAME)
+			#open it with read only privilages
+			for line in fd:
+				print line
