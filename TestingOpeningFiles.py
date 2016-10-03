2203333dd6750bbaaf253b69bda2ff5f44ed9bcc
DunnyDon <conordonohue@gmail.com>
2016-10-03 16:58:28 +0100

    Opens Airtel Data Files
    Completed Test where files and data can be opened and read

--- TestingOpeningFiles.py
+++ TestingOpeningFiles.py
@@ -1,5 +1,6 @@
 import os
-os.chdir("D:\Airtel-Chad-USSD-MO-CDR's-6months")
+#os.chdir("D:\Airtel-Chad-USSD-MO-CDR's-6months")
+root = "D:\Airtel-Chad-USSD-MO-CDR's-6months"
 for root, dirs, files in os.walk(root):
 	for FILENAME in files:
 		#make sure file is a log file and is called ProcessHistory
