89e48e7428d7ba022a10b06f40cbbd1465ae1cdc
DunnyDon <conordonohue@gmail.com>
2016-10-04 11:00:21 +0100

    Uploads Data to MongoDB
    Not throughly tested as of yet but it does seem to upload data correctly
    to the MongoDb database

--- TestingOpeningFiles.py
+++ TestingOpeningFiles.py
@@ -1,7 +1,12 @@
 import os, hashlib
+import pymongo
+from pymongo import MongoClient
+client = MongoClient()
 root = "D:\Airtel-Chad-USSD-MO-CDR's-6months"
 USSDRequestHeaders = ["SubscriberID","ServiceID","Transaction","Date","Time","CDRCorrelationID","MSISDN","IMSI","SessionID","USSDContent"]
 USSDResponseHeaders = ["SubscriberID","ServiceID","Transaction","Date","Time","CDRCorrelationID","MSISDN","SessionID","USSDContent","Status","ErrorCode"]
+db = client.FYP_Data_Storage
+collection = db.Airtel_Data
 for root, dirs, files in os.walk(root):
 	for FILENAME in files:
 		if not FILENAME.endswith('.gz'):
@@ -10,16 +15,21 @@ for root, dirs, files in os.walk(root):
 			#open it with read only privilages
 			for line in fd:
 				count = 0
-				print line.split(",")[2]
+				#print line.split(",")[2]
+				document_content = {}
 				if int(line.split(",")[2]) == 1:
 					for i in line.split(","):
 						if USSDRequestHeaders[count] == "IMSI":
 							i=hashlib.md5(i.encode()).hexdigest()
-						print USSDRequestHeaders[count]," ",i
+						document_content[USSDRequestHeaders[count]] = i
+						#print USSDRequestHeaders[count]," ",i
 						count+=1
 				else:
 					for i in line.split(","):
-						print USSDResponseHeaders[count]," ",i
+						#print USSDResponseHeaders[count]," ",i
+						document_content[USSDResponseHeaders[count]] = i
 						count+=1
-					break
-			break
+					#break
+				collection.insert_one(document_content)
+				#print document_content
+			#break
