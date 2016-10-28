799c0e8f184c779ab00a07fff2456afc0d5e5f11
DunnyDon <conordonohue@gmail.com>
2016-10-28 17:10:58 +0100

    Connect to Server
    Added comments to better describe the python script

--- ServerTest.py
+++ ServerTest.py
@@ -2,12 +2,16 @@ import pymongo
 from pymongo import MongoClient
 import re
 client = MongoClient('mongodb://user:password@host:port/db')
+# this is used for connecting to a remote MongoDb
 db = client.mongodb2706
 Tes = db.FYP_USSD_Codes
 document_cursor = Tes.find({}, {'_id':0})
 docs = document_cursor[:]
-
+#pass the findings into an array so they can be looped through
 def checkduplicates(s):
+	#search for the duplicates with # and their corresponding without
+	#this can be done by stripping # from the number and using regex
+	#to search for the number in the database
 	print "Searching for ", s.strip('#')
 	searchstring = re.compile(s.strip("#"))
 	test_cursor = Tes.find({"USSD":searchstring},{'_id':0})
@@ -20,5 +24,6 @@ for i in docs:
 	if "#" in i["USSD"]:
 		checkduplicates(i["USSD"])
 		countduplicates+=1
+		#count the number of duplicates
 
 print "There are ",countduplicates, " duplicates."
